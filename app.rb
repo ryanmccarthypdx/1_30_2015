7eddb04c99e46233e85c606093bcba4ed73af98d
Ryan <ryanmccarthypdx@gmail.com>
2015-01-30 11:20:50 -0800

    Add ability to add and list new bands

--- app.rb
+++ app.rb
@@ -5,12 +5,18 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
   @all_venues = Venue.all()
+  @all_bands = Band.all()
   erb(:main)
 end
 
 post('/add_venue') do
   new_venue_name = params.fetch("new_venue_name")
   Venue.create({:name => new_venue_name})
-  @all_venues = Venue.all()
+  redirect ('/')
+end
+
+post('/add_band') do
+  new_band_name = params.fetch("new_band_name")
+  Band.create({:name => new_band_name})
   redirect ('/')
 end
