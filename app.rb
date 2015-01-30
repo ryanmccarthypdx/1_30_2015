5999a2f23428e15c00b04833e97b0ad84f182c5e
Ryan <ryanmccarthypdx@gmail.com>
2015-01-30 11:51:39 -0800

    Add full functionality, ready to turn in

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 get('/') do
   @all_venues = Venue.all()
   @all_bands = Band.all()
+  @title = "Tour Manager"
   erb(:main)
 end
 
@@ -16,7 +17,14 @@ post('/add_venue') do
 end
 
 post('/add_band') do
+  @new_band_name = params.fetch("new_band_name")
+  @title = ("Book dates for ").concat(@new_band_name)
+  erb(:band)
+end
+
+post('/book_tour') do
+  venue_ids = params.fetch('venue_ids_array')
   new_band_name = params.fetch("new_band_name")
-  Band.create({:name => new_band_name})
+  Band.create({:name => new_band_name, :venue_ids => venue_ids})
   redirect ('/')
 end
