bad9215c5ee705f2bb6c58d14868f6cfcef2d292
Ryan <ryanmccarthypdx@gmail.com>
2015-01-30 10:38:13 -0800

    Make rspecs for validations and callbacks all green

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -1,1 +1,28 @@
 require('spec_helper')
+
+describe(Band) do
+
+  it{ should have_and_belong_to_many(:venues) }
+
+  describe('#titlecase_name') do
+    it('should put the band names in titlecase when creating') do
+      new_band = Band.create({:name => "rolling stones"})
+      expect(Band.find(new_band.id()).name()).to(eq("Rolling Stones"))
+    end
+  end
+
+  describe('#validates_uniqueness') do
+    it('should not save a duplicate band entry') do
+      new_band1 = Band.create({:name => "rolling stones"})
+      new_band2 = Band.create({:name => "Rolling Stones"})
+      expect(Band.all()).to(eq([new_band1]))
+    end
+  end
+
+  describe('#validates_presence') do
+    it('should not accept a blank name') do
+      new_band = Band.create({:name => ""})
+      expect(Band.all()).to(eq([]))
+    end
+  end
+end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -1,1 +1,28 @@
 require('spec_helper')
+
+describe(Venue) do
+
+  it{ should have_and_belong_to_many(:bands) }
+
+  describe('#titlecase_name') do
+    it('should put the venue names in titlecase when creating') do
+      new_venue = Venue.create({:name => "crystal ballroom"})
+      expect(Venue.find(new_venue.id()).name()).to(eq("Crystal Ballroom"))
+    end
+  end
+
+  describe('#validates_uniqueness') do
+    it('should not save a duplicate venue entry') do
+      new_venue1 = Venue.create({:name => "crystal ballroom"})
+      new_venue2 = Venue.create({:name => "Crystal Ballroom"})
+      expect(Venue.all()).to(eq([new_venue1]))
+    end
+  end
+
+  describe('#validates_presence') do
+    it('should not accept a blank name') do
+      new_venue = Venue.create({:name => ""})
+      expect(Venue.all()).to(eq([]))
+    end
+  end
+end
