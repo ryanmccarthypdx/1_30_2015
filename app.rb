6482c329cfa411e238fb85461a8b4c02674d7dea
Ryan <ryanmccarthypdx@gmail.com>
2015-01-30 11:14:26 -0800

    Add sinatra files and ability to add and list new venues

--- app.rb
+++ app.rb
@@ -2,3 +2,15 @@ require('bundler/setup')
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @all_venues = Venue.all()
+  erb(:main)
+end
+
+post('/add_venue') do
+  new_venue_name = params.fetch("new_venue_name")
+  Venue.create({:name => new_venue_name})
+  @all_venues = Venue.all()
+  redirect ('/')
+end
