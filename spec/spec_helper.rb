f8909d755f4788362f4e20ba469265030f82c02d
Ryan <ryanmccarthypdx@gmail.com>
2015-01-30 09:57:32 -0800

    Add all standard headers to spec_helper, app.rb, database.yml

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+ENV['RACK_ENV'] = 'test'
+
+require('bundler/setup')
+Bundler.require(:default, :test)
+
+Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Venue.all().each() do |venue|
+      venue.destroy()
+    end
+    Band.all().each() do |band|
+      band.destroy()
+    end
+  end
+end
